d9fa8a11c5af5c4bd3f33752de1c630e679bde0e
Khanh Hoa <hoa.lokhanh@amela.vn>
2023-04-25 13:55:23 +0700

    add page detail post

--- helper/index.ts
+++ helper/index.ts
@@ -20,4 +20,35 @@ export const convertImages = (htmlText: string) =>
     '<div style="text-align:center;"><img'
   );
 
+export const checkScript = (htmlText: string) => {
+  const SCRIPT_REGEX = /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+  const testScript = SCRIPT_REGEX.test(htmlText);
+
+  return testScript;
+};
+
+export const replaceURLs = (message: string) => {
+  if (!message) return;
+
+  var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+  return message.replace(urlRegex, function (url) {
+    var hyperlink = url;
+    if (!hyperlink.match("^https?://")) {
+      hyperlink = "http://" + hyperlink;
+    }
+    return (
+      '<a href="' +
+      hyperlink +
+      '" target="_blank" rel="noopener noreferrer">' +
+      url +
+      "</a>"
+    );
+  });
+};
+
+export const handleError = (err: string) => {
+  message.destroy();
+  message.error(err);
+};
+
 export const imgMaxSize = 5000000;
